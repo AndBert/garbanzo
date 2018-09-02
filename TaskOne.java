2b25177a1d738426c5736a0cb49ebb1c59063fd0
AndBert <42816028+AndBert@users.noreply.github.com>
2018-09-02 21:48:42 +0300

    Add files via upload

---
+++ TaskOne.java
@@ -0,0 +1,25 @@
+
+public class TaskOne {
+
+	public static void main(String[] args) {
+
+		int sec = 4_500_000;
+		int min = sec/60;
+		int hour = min/60;
+		int day = hour/24;
+		int week = day/7;
+		int secRest = sec%60;
+		int minRest = min%60;
+		int hourRest = hour%24;
+		int dayRest = day%7;
+		
+	System.out.println(sec + " second is " + min + " full minutes and " + secRest + " seconds ");
+	System.out.println("OR");
+	System.out.println(sec + " second is " + hour + " full hours and " + minRest + " full minutes and " + secRest + " seconds ");
+	System.out.println("OR");
+	System.out.println(sec + " second is " + day + " full days and " + hourRest + " full hours and " + minRest + " full minutes and " + secRest + " seconds ");
+	System.out.println("OR");
+	System.out.println(sec + " second is " + week + " full weeks and " + dayRest + " full days and " + hourRest + " full hours and " + minRest + " full minutes and " + secRest + " seconds ");
+	}
+
+}
